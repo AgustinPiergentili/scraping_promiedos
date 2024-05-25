63a35817218e6366cb3b071f380d458fc34b521f
Piergen <agus.piergentili73@gmail.com>
2024-05-25 12:33:13 -0300

    se corrien algunos errores, y se sube el resto de los metodos de los resultados de los partidos

--- pruebaScrap.py
+++ pruebaScrap.py
@@ -17,55 +17,66 @@ class Argentina():
 
 
 class Argentina:
+    """Clase para vizualizar los resultados de Argentina en el mata-mata"""
     def __init__(self):
         self.soup = soup
         self.divs = soup.find_all("div", id="py2restotal")
 
     def impirmir_info(self):
+        """Metodo para ver los divs"""
         return self.divs
 
     def listar_divs(self):
+        """Metodo para ver los divs en una lista"""
         for div in self.divs:
             print(div)
             print("\n" + "-"*50 + "\n")  # Separador para mayor legibilidad
 
     def resultado_octavos(self):
-        """Busca el resultado en la pagina"""
+        """Metodo para buscar el resultado de octavos"""
         if len(self.divs) >= 2:
             resultado = self.divs[1].get_text(strip=True)
             return resultado
 
     def imprimir_octavos(self):
-        """Imprime el resultado de octavos"""
+        """Metodo que imprime el resultado de octavos"""
         resultado = self.resultado_octavos()
         return f"Argentina {resultado} Australia"
 
     def resultado_cuartos(self):
+        """Metodo para buscar el resultado de cuartos"""
         if len(self.divs) >= 9:
             return self.divs[8].get_text(strip=True)
 
     def imprimir_cuartos(self):
+        """Metodo que imprime el resultado de cuartos"""
         resultado = self.resultado_cuartos()
         if resultado == "23-42":
             return f"Argentina 2 (4) - (3) 2  Paises Bajos"
 
+    def resultado_semifinal(self):
+        """Metodo para buscar el resultado de semis"""
+        if len(self.divs) >= 13:
+            return self.divs[12].get_text(strip=True)
+        
+    def imprimir_semis(self):
+        resultado = self.resultado_semifinal()
+        return f"Argentina {resultado} Croacia"
 
-
+    def resultado_final(self):
+        if len(self.divs) >= 15:
+            return self.divs[14].get_text(strip=True)
+        
+    def imprimir_final(self):
+        resultado = self.resultado_final()
+        if resultado == "34-23":
+            return f"Argentina 3 (4) - (2) 3 Francia"
+    
+    
+    
+    
+    
 argentina = Argentina()
 
-print(argentina.imprimir_cuartos())
+print(argentina.imprimir_final())
 
-
-#print(argentina_octavos)
-
-#src_todos = soup.find_all(src=True)
-
-
-
-#for i, imagen in enumerate(src_todos):
-    #if imagen ["src"].endswith("png"):
-        #print(imagen["src"])
-        #r = requests.get(f"https://scrapepark.org/courses/spanish/{imagen['src']}")
-        
-        #with open(f"imagen_{i}.png", "wb") as f:
-            #f.write(r.content)
